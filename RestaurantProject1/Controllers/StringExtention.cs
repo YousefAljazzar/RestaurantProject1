3edcb8d71e7d021286f4a1bb7c1204008b97f1b8
YousefAljazzar <yousefornot@hotmail.com>
2022-09-15 23:43:27 +0300

    Add extention

---
+++ RestaurantProject1/Controllers/StringExtention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantProject1.Controllers
+{
+    public static class StringExtention
+    {
+        public static string capitalizeFirstChar(this string name)
+        {
+            return char.ToUpper(name[0]) + name.Substring(1);
+        }
+    }
+}
